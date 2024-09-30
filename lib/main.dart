7c5b00c2e144af430e9824236f3f249a59057486
Zerfa <zerfa@naver.com>
2024-09-30 17:59:13 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -25,35 +28,71 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-          children: [
-            Text("Recipes", style: TextStyle(fontSize: 30)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _mIcon(Icons.food_bank, "ALL"),
-                SizedBox(width: 25),
-                _mIcon(Icons.emoji_food_beverage, "ALL"),
-                SizedBox(width: 25),
-                _mIcon(Icons.fastfood, "ALL"),
-                SizedBox(width: 25),
-                _mIcon(Icons.local_pizza, "ALL"),
-              ],
-            )
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back_ios),
+          title: Text("제목자리"),
+          actions: [
+            Icon(Icons.search),
+            SizedBox(width: 16),
+            Icon(CupertinoIcons.heart, color: Colors.redAccent),
+            //보통 앱의 기본 여백공간은 16이므로
+            SizedBox(width: 16)
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ListView(
+            children: [
+              _title(),
+              SizedBox(height: 10),
+              _menu(),
+              SizedBox(height: 10),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Placeholder(),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+            ],
+          ),
         ));
   }
 
+  Text _title() {
+    return Text("Recipes",
+        style: GoogleFonts.patuaOne(
+          textStyle: TextStyle(fontSize: 30),
+        ));
+  }
 
-  //매개변수에 타입을 안 적으면 dynamic으로 타입추론을 한다.
-  Container _mIcon(mIcon, text) {
+  Row _menu() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _mIcon(Icons.food_bank, "ALL"),
+        SizedBox(width: 25),
+        _mIcon(Icons.emoji_food_beverage, "ALL"),
+        SizedBox(width: 25),
+        _mIcon(Icons.fastfood, "ALL"),
+        SizedBox(width: 25),
+        _mIcon(Icons.local_pizza, "ALL"),
+      ],
+    );
+  }
+
+  //매개변수에 타입을 안 적으면 dynamic으로 타입추론을 하지만
+  //우리가 만들어서 알고 있지 남들은 모른다. 매개변수 타입은 배려를 위해 써주자.
+  //선택적 매개변수는? 안 받아도 되는 것들 혹은 매개변수가 매우 많을 때 사용.
+  Container _mIcon(IconData mIcon, String text) {
     return Container(
       width: 60,
       height: 60,
       decoration: BoxDecoration(
           border: Border.all(color: Colors.black12),
-          borderRadius: BorderRadius.circular(30)
-      ),
+          borderRadius: BorderRadius.circular(30)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -62,7 +101,9 @@ class HomePage extends StatelessWidget {
             color: Colors.redAccent,
             size: 30,
           ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           Text(
             //문자열이 null이면 터지기 때문에 "" 안에 $변수로 넣어준다.
             "$text",
